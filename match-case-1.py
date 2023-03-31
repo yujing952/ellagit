0c3b22a33164bf53946330d8e31a373f7c6a7b46
Yujing <niuyujing2021@163.com>
2023-03-31 21:37:17 +0800

    python study

--- match-case-1.py
+++ match-case-1.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 
+print("======= case1 =========")
 def http_error(status):
     match status:
         case 400:
@@ -12,5 +13,67 @@ def http_error(status):
             print("something wrong with internet")
             
 mystatus = 400
-http_error(mystatus)        
-                
+http_error(mystatus)  
+
+#===================
+print("======== case2 ========")
+
+m = 0
+while m < 10:
+    print("the count %d is less than 10"% m)
+    m = m + 1
+else:
+    print("the count %d is equal to or bigger than 10" % m)      
+    
+#===================
+print("======== case3 ========")
+
+sites = ["google","baidu","taobao","tiaomao"]
+for site in sites:
+    if site == "taobao":
+        print("the sie is: %s" % site)  
+        break
+    print("the site is %s" % site)
+else:
+    print("there is no data on loop")
+print("the loop is done")              
+ 
+#===================
+print("======== case4 ========")   
+word = "beautiful"
+for letter in word:
+    print(letter)
+    
+#===================
+print("======== case5 ========")  
+for i in range(1,6):
+    print(i)
+    
+#===================
+print("======== case6 ========")     
+for j in range(6):
+    print(j) 
+else:
+    print("game over")
+    
+    
+    
+#===================
+print("======== case7 ========")
+
+sites = ["google","baidu","taobao","tiaomao"]
+for l in range(len(sites)):
+    print(l,sites[l])
+    
+ 
+ #===================
+print("\n======== case8 ========")   
+for char in 'PYTHON STRING':
+  if char == ' ':
+      break
+
+  print(char, end='')
+  if char == 'O':
+      continue
+
+  
