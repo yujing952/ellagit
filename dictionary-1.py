c4e46402621c758ffb25f678eece7c4397a6c8fe
Yujing <niuyujing2021@163.com>
2023-03-31 10:42:50 +0800

    backup python study

---
+++ dictionary-1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+dict = {}
+dict['one'] = "1 - Cainiao course"
+dict[2] = "2 - Cainiao tools"
+tinydict = {'name':'runoob','code':1,'site':"www.runoob.com"}
+
+print(dict['one'])
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
+
